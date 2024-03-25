f5940a8782eda07aac262ed70142c815ae68e08b
JuliaHusar <juliahusar03@gmail.com>
2024-03-25 13:20:04 -0400

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import React from 'react'
 import {Title} from "./components/TitleComponent.jsx";
 import {DownButton} from "./components/DownButtonComponent.jsx";
 import {Description} from "./components/Description.jsx";
