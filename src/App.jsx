c0469565a9bf51c84afa3a548c70536c87cdb99d
JuliaHusar <juliahusar03@gmail.com>
2024-08-15 20:54:50 -0400

    Changed Hompage to actual homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-
-import './App.css'
-import {Title} from "./components/TitleComponent.jsx";
-import {DownButton} from "./components/DownButtonComponent.jsx";
-import {Description} from "./components/Description.jsx";
-import {ProjectContainer} from "./components/ProjectContainer.jsx";
-import {Navbar} from "./components/Navbar.jsx";
-import {Contact} from "./components/Contact.jsx";
+import './App.css';
+import { Title } from "./components/TitleComponent.jsx";
+import { DownButton } from "./components/DownButtonComponent.jsx";
+import { Description } from "./components/Description.jsx";
+import { ProjectContainer } from "./components/ProjectContainer.jsx";
+import { Navbar } from "./components/Navbar.jsx";
+import { Contact } from "./components/Contact.jsx";
 import AboutMe from "./components/AboutMe.jsx";
-import {ComputerIcon} from "./components/ComputerIcon.jsx";
-import {
-    BrowserRouter,
-    createBrowserRouter, Route,
-    RouterProvider, Routes,
-} from "react-router-dom";
-import {RuinTheAuxPrivacy} from "./components/RuinTheAuxPrivacy.jsx";
+import { ComputerIcon } from "./components/ComputerIcon.jsx";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { RuinTheAuxPrivacy } from "./components/RuinTheAuxPrivacy.jsx";
 
 function App() {
     window.addEventListener('scroll', function() {
         const navbar = document.querySelector('.navbar');
         const scrollHeight = window.document.body.scrollHeight;
         const scrollPosition = window.scrollY;
-        const opacity = (scrollPosition / scrollHeight)*3;
-        if(navbar){
+        const opacity = (scrollPosition / scrollHeight) * 3;
+        if (navbar) {
             navbar.style.opacity = opacity;
         }
     });
 
-
-
     return (
-        <BrowserRouter basename="/portfolio-website-2024">
+        <HashRouter>
             <Routes>
                 <Route path="/" element={
                     <div>
@@ -57,8 +50,8 @@ function App() {
                 } />
                 <Route path="/RuinTheAuxPrivacyPolicy" element={<RuinTheAuxPrivacy />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
-export default App
+export default App;
