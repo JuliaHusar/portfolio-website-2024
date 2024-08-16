33c74ed08b2c7a5e233e7fd46806e35d2c037e68
JuliaHusar <juliahusar03@gmail.com>
2024-08-15 20:16:00 -0400

    Changed Hompage to actual homepage

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {WebRouting} from "./WebRouting.jsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
 import {RuinTheAuxPrivacy} from "./components/RuinTheAuxPrivacy.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <BrowserRouter>
+      <Router basename='portfolio-website-2024'>
           <Routes>
-              <Route path="/portfolio-website-2024" element={<App />} />
+              <Route path="/" element={<App />} />
               <Route path="/RuinTheAuxPrivacyPolicy" element={<RuinTheAuxPrivacy />} />
           </Routes>
-      </BrowserRouter>
+      </Router>
   </React.StrictMode>,
 )
